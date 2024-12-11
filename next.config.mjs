0b8bfafae219bf40e2a085a13ac04209ee414aa8
dylancoyne <dylan@dylancoyne.co.uk>
2024-12-11 20:28:17 +0000

    Adding in next

--- next.config.mjs
+++ next.config.mjs
@@ -19,27 +19,6 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
-}
-
 function remarkMDXLayout(source, metaName) {
   let parser = Parser.extend(jsx())
   let parseOptions = { ecmaVersion: 'latest', sourceType: 'module' }
