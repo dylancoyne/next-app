9a861789a697167c86c6599e2e09136151c45bab
Dylan Coyne <dylan@dylancoyne.co.uk>
2024-12-12 17:26:45 +0000

    Body color change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,9 +22,22 @@ export default {
         '4xl': '2.5rem',
       },
       colors: {
-        'text-neutral-950': '#191818',
-        'bg-neutral-950': '#191818',
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'dark': {
+          100: '#cffafe',
+          200: '#a5f3fc',
+          300: '#67e8f9',
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2',
+          700: '#0e7490',
+          800: '#155e75',
+          900: '#164e63',
+          950: '#191818',
       },
+    },
       fontFamily: {
         sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
         display: [
