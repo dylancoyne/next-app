f7c1473bfb4d8a0a28979165d0d5c295b9dd78ce
dylancoyne <dylan@dylancoyne.co.uk>
2024-12-12 07:26:08 +0000

    Removed files

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"agwd17JyWX68QG07nimUBBbZW9n24th1kYuzoU6x/14=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
