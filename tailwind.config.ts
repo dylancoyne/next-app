7013bcc112703e38870aaf25a38036238d5decf7
Dylan Coyne <dylan@dylancoyne.co.uk>
2024-12-13 08:18:05 +0000

    updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,7 +26,7 @@ export default {
         current: 'currentColor',
         'white': '#ffffff',
         'neutral': {
-          100: '#cffafe',
+          /**100: '#cffafe',
           200: '#a5f3fc',
           300: '#67e8f9',
           400: '#22d3ee',
@@ -34,7 +34,7 @@ export default {
           600: '#0891b2',
           700: '#0e7490',
           800: '#155e75',
-          900: '#164e63',
+          900: '#164e63',**/
           950: '#191818',
       },
     },
